4048303925b1d2042717acd6a99ee89f16f3cb65
Steven Jackson <hpats67@gmail.com>
2017-05-13 17:58:49 -0700

    JSON turned to C# objects

--- Program.cs
+++ Program.cs
@@ -2,13 +2,11 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 
 namespace WebAPIClient
 {
-    public class repo
-    {
-        public string name;
-    }
     class Program
     {
         static void Main(string[] args)
@@ -19,15 +17,16 @@ namespace WebAPIClient
         private static async Task ProcessRepositories()
         {
             var client = new HttpClient();
+            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".Net Foundation Repository Reporter");
             
-            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
 
-            var msg = await stringTask;
-            Console.Write(msg);
+            foreach(var repo in repositories) Console.WriteLine(repo.name);
         }
         
     }
--- repo.cs
+++ repo.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization.Json;
 
 namespace WebAPIClient
 {
   public class repo
   {
     public string name;
-    var serializer = new DataContractJsonSerializer(typeof(List<repo>));
   }
 }
